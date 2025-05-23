78263bbb26c5b9defbaffeeabe7fa45f15c4bef1
mira <mirasuper99@gmail.com>
2025-05-23 21:56:59 +0200

    1

---
+++ src/basic/6.ts
@@ -0,0 +1,30 @@
+// Оголошення інтерфейсу User
+interface Address {
+    city: string;
+    country: string;
+  }
+  
+  interface User {
+    name: string;
+    age: number;
+    email: string;
+    address?: Address; // Необов'язкова властивість
+  }
+  
+  // Типізовані об'єкти
+  const mango: User = {
+    name: 'Mango',
+    age: 30,
+    email: 'john@example.com',
+    address: {
+      city: 'New York',
+      country: 'USA'
+    }
+  };
+  
+  const poly: User = {
+    name: 'Mango',
+    age: 30,
+    email: 'john@example.com'
+  };
+  
---
+++ src/generics/2.ts
@@ -0,0 +1,24 @@
+// src/generics/2.ts
+
+type AllType = {
+    name: string;
+    position: number;
+    color: string;
+    weight: number;
+  }
+  
+  function compare <
+    T extends Pick<AllType, 'name' | 'color'>,
+    B extends Pick<AllType, 'position' | 'weight'>
+  >(
+    top: T,
+    bottom: B
+  ): AllType {
+    return {
+      name: top.name,
+      color: top.color,
+      position: bottom.position,
+      weight: bottom.weight,
+    };
+  }
+  
